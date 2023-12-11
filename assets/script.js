a80d8d5003120383cf3fd0f5c415025296dd6ffc
unknown <whritenourc@outlook.com>
2023-12-11 16:57:22 -0600

    added the structure of the quiz to the html and added variables in js

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,13 @@
 var quizEl = document.querySelector("#quiz");
 var startQuizEl = document.querySelector("#start-quiz");
 var timerEL = document.querySelector("#timer");
+var answersEl = document.querySelector("#answers")
+var answer1El = document.querySelector("#answer1")
+var answer2El = document.querySelector("#answer2")
+var answer3El = document.querySelector("#answer3")
+var answer4El = document.querySelector("#answer4")
+
+var questions = ["Commonly used data types DO not Include:", "The condition in an if"]
 
 var secondsLeft = 101;
 
